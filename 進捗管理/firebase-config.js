8245c822097baaa746b0d5eb32eb6852e392be1e
0815cinnamon-a11y <0815cinnamon@gmail.com>
2026-03-16 07:54:08 +0900

    Update firebase-config.js

--- 進捗管理/firebase-config.js
+++ 進捗管理/firebase-config.js
@@ -1,10 +1,13 @@
-﻿window.FLOWLOG_FIREBASE = {
-  enabled: false,
-  projectId: "",
-  appId: "flowlog-default",
+window.FLOWLOG_FIREBASE = {
+  enabled: true,
+  projectId: "flowlog-b4d48",
+  appId: "1:194159466417:web:a7eb0bf0d27986f7b70f90",
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
+    apiKey: "AIzaSyAQJ7iuxRrNUPgJIUVu3mUFlXf3Oyu i6CY".replace(" ", ""),
+    authDomain: "flowlog-b4d48.firebaseapp.com",
+    projectId: "flowlog-b4d48",
+    storageBucket: "flowlog-b4d48.firebasestorage.app",
+    messagingSenderId: "194159466417",
+    appId: "1:194159466417:web:a7eb0bf0d27986f7b70f90",
   },
 };
